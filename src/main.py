0016f0097e586baba4bbfb0e93870f2e0b28f941
Amir Ehsandar <ehsandaramir@gmail.com>
2023-09-21 14:09:19 +0330

    Enable CORS plugin

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.sources.router import router as sources_router
 from src.users.crypto.jwt import JsonWebToken
@@ -10,3 +11,13 @@ app = FastAPI(title="pgforms")
 
 app.include_router(router=users_router)
 app.include_router(router=sources_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
