3b75e4db772568eebffdccfc9ac684602525118b
Amir Ehsandar <ehsandaramir@gmail.com>
2023-10-16 23:35:19 +0330

    Remove react

--- pgforms/src/app/logout/page.js
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import {redirect} from "next/navigation";
-
-export default function Page() {
-  localStorage.removeItem("user")
-  localStorage.removeItem("jwt")
-  redirect("/login")
-}
